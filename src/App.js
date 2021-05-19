a7aa78515b8569a09427bc1bec5f57c5237f2bd0
Kushagra <kushag44@yahoo.com>
2021-05-19 18:57:29 +0530

    Page 2 popup

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import './App.css';
 import System from './pages/system';
 import ManageCodes from './pages/manageCodes';
 import EditCode from './pages/editCode';
+import AddCodeValue from './pages/addCodeValue';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-        <Route path="/editcode">
-            <EditCode />
+          {/*<Route path="/addcodevalue">
+            <AddCodeValue />
           </Route>
+          <Route path="/editcode">
+            <EditCode />
+          </Route>*/}
           <Route path="/managecodes">
             <ManageCodes />
           </Route>
